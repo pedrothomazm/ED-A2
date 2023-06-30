b5b24f84d852695f2b0a5f4bee18a1aebe9cfcdc
Tatiana Lage <tatiana.lage28@gmail.com>
2023-06-29 22:31:17 -0300

    Adicionar arquivo tree.h

---
+++ tree.h
@@ -0,0 +1,20 @@
+#ifndef TREE_H
+#define TREE_H
+
+struct TreeNode
+{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+TreeNode* createNode(int data);
+TreeNode* insertNode(TreeNode* root, int data);
+TreeNode* buildTreeFromUserInput();
+int getHeight(TreeNode* root);
+int getSize(TreeNode* root);
+void insertElement(TreeNode*& root, int data);
+TreeNode* findMin(TreeNode* node);
+TreeNode* removeNode(TreeNode* root, int data);
+
+#endif
