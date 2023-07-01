6f67b28afd9af5ecdce7fdd35c8c36843424a20d
Tatiana Lage <tatiana.lage28@gmail.com>
2023-06-30 22:24:13 -0300

    Atualize tree.h

--- tree.h
+++ tree.h
@@ -1,6 +1,8 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <string>
+
 struct TreeNode
 {
     int idata;
@@ -10,10 +12,12 @@ struct TreeNode
 
 TreeNode* createNode(int idata);
 TreeNode* insertNode(TreeNode* ptrRoot, int idata);
+TreeNode* buildTreeFromFile(const std::string& filename);
 TreeNode* buildTreeFromUserInput();
 int getHeight(TreeNode* ptrRoot);
 int getSize(TreeNode* ptrRoot);
 void insertElement(TreeNode*& ptrRoot, int idata);
+void printInOrder(TreeNode* ptrRoot);
 TreeNode* findMin(TreeNode* ptrNode);
 TreeNode* removeNode(TreeNode* ptrRoot, int idata);
 void deleteTree(TreeNode*& ptrRoot);
